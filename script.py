7faac65ec2fed41c831ec3bd88328d39dc695a54
yannay123 <yan.comsci@gmail.com>
2023-01-23 21:52:44 +0800

    remove some code

--- script.py
+++ script.py
@@ -1,17 +1,4 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-# print(sys.version)
-print(sys.executable)
-
-
-def greet(who_to_greet):
-
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
